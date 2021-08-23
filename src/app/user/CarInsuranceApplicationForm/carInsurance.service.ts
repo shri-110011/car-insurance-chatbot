99a0f4b4e72be9b99cb63e1ee44c7146bb88829d
Shrikant Desktop <shrikant.test.accnt@gmail.com>
2021-08-23 18:01:50 +0530

    Updated getDate() in carInsurance and chat service to handle the AM/PM issue

--- src/app/user/CarInsuranceApplicationForm/carInsurance.service.ts
+++ src/app/user/CarInsuranceApplicationForm/carInsurance.service.ts
@@ -40,6 +40,22 @@ export class CarInsuranceService {
         var month = x.getMonth()<10? 0+""+(x.getMonth()+1): (x.getMonth()+1);
         var date = x.getDate()<10? 0+""+x.getDate(): x.getDate();
         var time = x.toLocaleTimeString();
+        if(time.includes("PM")) {
+            if(parseInt(time.split(":")[0])!=12) {
+                time=(parseInt(time.charAt(0))+12)+time.substring(1, time.indexOf('PM')-1);
+            }
+            else {
+                time.substring(0, time.indexOf('PM')-1)
+            }
+        }
+        else if(time.includes("AM")) {
+            if(parseInt(time.split(":")[0])!=12) {
+                time=time.substring(0, time.indexOf('AM')-1);
+            }
+            else {
+                time="00"+time.substring(2, time.indexOf('AM')-1);
+            }
+        }
         return year+"-"+month+"-"+date+" "+time;
     }
     
